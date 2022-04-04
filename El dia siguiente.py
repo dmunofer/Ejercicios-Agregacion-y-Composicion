b2d2543d260067a83d05ab12cb042e0bdd0791db
dmunofer <diego.fmz@hotmail.com>
2022-04-04 22:32:08 +0200

    ejemplo añadido

--- El dia siguiente.py
+++ El dia siguiente.py
@@ -11,6 +11,18 @@ class Edificio(Ciudad):
     def __init__(self, edificio):
         self.edificio = edificio
 
-class Empleado(Edificio):
+class Empleado(Empresa):
     def __init__(self, empleado):
         self.empleado = empleado
+
+
+
+YooHoo = Empresa('YooHoo!')
+New_York = Ciudad()
+Los_Angeles = Ciudad()
+A = Edificio(New_York)
+B = Edificio(New_York)
+C = Edificio(Los_Angeles)
+Sres_Martin = Empleado(YooHoo)
+Salim = Empleado(YooHoo)
+Sra_Xing = Empleado(YooHoo)
